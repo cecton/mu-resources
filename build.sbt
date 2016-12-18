a2d194e22b5da65a7389b56018498b9066479fd6
Cecile Tonglet <cecile.tonglet@gmail.com>
2016-12-18 12:33:09 +0100

    Switch to ScalaTest

--- build.sbt
+++ build.sbt
@@ -22,10 +22,11 @@ resolvers += Classpaths.typesafeReleases
 libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra" % ScalatraVersion,
   "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-  "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+  "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
   "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
   "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
-  "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
+  "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+  "co.blocke" %% "scalajack" % "4.8.3_5925c8_RC"
 )
 
 scalateTemplateConfig in Compile := {
--- src/test/scala/com/cecton/mu_resources/MuResourcesSpec.scala
+++ src/test/scala/com/cecton/mu_resources/MuResourcesSpec.scala
@@ -1,16 +1,15 @@
 package com.cecton.mu_resources
 
-import org.scalatra.test.specs2._
+import org.scalatest.FunSuiteLike
+import org.scalatra.test.scalatest._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class MuResourcesSpec extends ScalatraSpec { def is =
-  "GET / on MuResources"                     ^
-    "should return status 200"                  ! root200^
-                                                end
-
+class MuResourcesSpec extends ScalatraSuite with FunSuiteLike {
   addServlet(classOf[MuResources], "/*")
 
-  def root200 = get("/") {
-    status must_== 200
+  test("GET / on MuResources") {
+    get("/") {
+      status should equal(200)
+    }
   }
 }
