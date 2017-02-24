5c32c6d655f539a2149d490d5b2b647d1ffc15d5
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-02-24 19:23:24 +0100

    WIP Attempt to fetch relationships from SPARQL

--- src/main/scala/com/cecton/mu_resources/dbadapters/SPARQL.scala
+++ src/main/scala/com/cecton/mu_resources/dbadapters/SPARQL.scala
@@ -7,7 +7,7 @@ import io.circe.parser._
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success, Try}
 
-import com.cecton.mu_resources.resource.Resource
+import com.cecton.mu_resources.resource._
 
 trait SPARQL {
   val selectReq: Req
@@ -18,24 +18,51 @@ trait SPARQL {
   case class JSONRow(val s: JSONValue, val p: JSONValue, val o: JSONValue)
   case class JSONValue(val `type`: String, val value: Json)
 
+  case class ResourceContext(val id: String, val items: Map[String, Seq[JSONRow]]) {
+    val me = items(id)
+  }
+  case class RelationshipsContext(val resources: Map[String, Resource], val bindings: Seq[JSONRow])
+
   trait ResourceAdapter {
 
-    def toResource: Seq[JSONRow] => Resource
+    def toResource: ResourceContext => Resource
 
-    def getString(p: String)(implicit rows: Seq[JSONRow]): Option[String] = rows
-      .filter { x => x.p.value.asString.get == p }
-      .headOption
-      .flatMap { x => x.o.value.asString }
+    def toRelationships: RelationshipsContext => Seq[Seq[Relationship]]
 
-    def list()(implicit ctx: ExecutionContext): Future[Iterable[Resource]] = {
-      val prom = Promise[Iterable[Resource]]()
+    def getString(p: String)(implicit context: ResourceContext): Option[String] =
+      context.me
+        .collect {
+          case x if x.p.value.asString == Some(p) => x.o.value.asString
+        }
+        .flatten
+        .headOption
+
+    def getOne2One(p: String)(implicit context: RelationshipsContext): Seq[One2One] = {
+      context
+        .bindings
+        .filter(_.p.value.asString == Some(p))
+        .map(x => (x.s.value.asString, x.o.value.asString))
+        .collect {
+          case (Some(x), Some(y)) =>
+            One2One(context.resources(x), context.resources(y))
+        }
+    }
+
+    def list()(implicit ctx: ExecutionContext): Future[(Iterable[Resource], Relationships)] = {
+      val prom = Promise[(Iterable[Resource], Relationships)]()
       val request = selectReq
         .addQueryParameter("query", "select * {?s a <something> ; ?p ?o}")
       dispatch.Http(request OK as.String) onComplete {
         case Success(content) =>
           val bindings = decode[JSONResponse](content).right.get.results.bindings
-          val items = bindings.groupBy(_.s.value.asString.get).values
-          prom.complete(Try(items.map { rows => toResource(rows) }))
+          val items = bindings.groupBy(_.s.value.asString.get)
+          val resources: Map[String, Resource] = items.map { x =>
+            val context = ResourceContext(x._1, items)
+            (x._1, toResource(context))
+          }
+          val context = RelationshipsContext(resources, bindings)
+          val relationships = Relationships(toRelationships(context).flatten:_*)
+          prom.complete(Try((resources.values, relationships)))
         case Failure(exception) =>
           println(exception)
           prom.failure(exception)
--- src/test/scala/com/cecton/mu_resources/dbadapters/SPARQLSpec.scala
+++ src/test/scala/com/cecton/mu_resources/dbadapters/SPARQLSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.AsyncFlatSpec
 import org.scalatest.Matchers._
 import scala.util.Try
 
-import com.cecton.mu_resources.resource.Resource
+import com.cecton.mu_resources.resource._
 
 class SPARQLSpec extends AsyncFlatSpec {
 
@@ -38,9 +38,19 @@ class SPARQLSpec extends AsyncFlatSpec {
           |        "o": { "type": "literal" , "value": "Harry Potter and the Order of the Phoenix" }
           |      } ,
           |      {
+          |        "s": { "type": "uri" , "value": "http://example.org/book/book5" } ,
+          |        "p": { "type": "uri" , "value": "http://example.org/book/next" } ,
+          |        "o": { "type": "uri" , "value": "http://example.org/book/book6" }
+          |      } ,
+          |      {
           |        "s": { "type": "uri" , "value": "http://example.org/book/book4" } ,
           |        "p": { "type": "uri" , "value": "http://example.org/book/title" } ,
           |        "o": { "type": "literal" , "value": "Harry Potter and the Goblet of Fire" }
+          |      } ,
+          |      {
+          |        "s": { "type": "uri" , "value": "http://example.org/book/book4" } ,
+          |        "p": { "type": "uri" , "value": "http://example.org/book/next" } ,
+          |        "o": { "type": "uri" , "value": "http://example.org/book/book5" }
           |      } ,
           |      {
           |        "s": { "type": "uri" , "value": "http://example.org/book/book3" } ,
@@ -48,14 +58,29 @@ class SPARQLSpec extends AsyncFlatSpec {
           |        "o": { "type": "literal" , "value": "Harry Potter and the Prisoner Of Azkaban" }
           |      } ,
           |      {
+          |        "s": { "type": "uri" , "value": "http://example.org/book/book3" } ,
+          |        "p": { "type": "uri" , "value": "http://example.org/book/next" } ,
+          |        "o": { "type": "uri" , "value": "http://example.org/book/book4" }
+          |      } ,
+          |      {
           |        "s": { "type": "uri" , "value": "http://example.org/book/book2" } ,
           |        "p": { "type": "uri" , "value": "http://example.org/book/title" } ,
           |        "o": { "type": "literal" , "value": "Harry Potter and the Chamber of Secrets" }
           |      } ,
           |      {
+          |        "s": { "type": "uri" , "value": "http://example.org/book/book2" } ,
+          |        "p": { "type": "uri" , "value": "http://example.org/book/next" } ,
+          |        "o": { "type": "uri" , "value": "http://example.org/book/book3" }
+          |      } ,
+          |      {
           |        "s": { "type": "uri" , "value": "http://example.org/book/book1" } ,
           |        "p": { "type": "uri" , "value": "http://example.org/book/title" } ,
           |        "o": { "type": "literal" , "value": "Harry Potter and the Philosopher's Stone" }
+          |      } ,
+          |      {
+          |        "s": { "type": "uri" , "value": "http://example.org/book/book1" } ,
+          |        "p": { "type": "uri" , "value": "http://example.org/book/next" } ,
+          |        "o": { "type": "uri" , "value": "http://example.org/book/book2" }
           |      }
           |    ]
           |  }
@@ -76,8 +101,12 @@ class SPARQLSpec extends AsyncFlatSpec {
   }
 
   case object ResourceBook extends MockDatabase.ResourceAdapter {
-    def toResource = { implicit rows =>
+    def toResource = { implicit context =>
       Book(getString("http://example.org/book/title"))
+    }
+
+    def toRelationships = { implicit context =>
+      Seq(getOne2One("http://example.org/book/next"))
     }
   }
 
@@ -85,7 +114,13 @@ class SPARQLSpec extends AsyncFlatSpec {
 
   it should "list existing resources" in {
     val res = ResourceBook.list
-    res map { r => assert(r.size == 6) }
+    res map { r =>
+      val books = r._1
+      val relationships = r._2
+      assert(books.size == 6)
+      assert(relationships.map.size == 6)
+      assert(relationships.map.values.flatten.size == 10)
+    }
   }
 
 }
